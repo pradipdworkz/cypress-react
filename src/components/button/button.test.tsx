4fe7b796b172677108d524cfe01257ff7446e1b4
pradip <pradipd@workz.com>
2021-10-05 15:51:42 +0400

    customize button component

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -3,12 +3,9 @@ import Button from './index';
 
 describe("Button Component", () => {
     it("button should contain text", () => {
-        let value = 1
         const props = {
             title: "-",
-            onclick: () => {
-                value = value + 1
-            }
+            onclick: () => { }
         }
         mount(<Button {...props} />)
         cy.get("[data-cy='button-component']").should('contain', props.title)
